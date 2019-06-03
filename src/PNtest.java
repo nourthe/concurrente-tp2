262f4746a602416962e553638c5ce3841152bb86
Bruno <bruno178pm@gmail.com>
2019-06-02 22:59:47 -0300

    Arregla error en el test.

--- src/PNtest.java
+++ src/PNtest.java
@@ -29,22 +29,22 @@ class PNtest {
 			System.out.println("No se puede consumir en buffer 2");
 		}
 
-		for (int i = 0; i<14; i++) {
-			if (pene.isTransitionEnabled(PN.Transitions.CONSUME_BUFFER_1)) {
-				System.out.println("Puedo consumir buffer 1");
-				pene.fire(PN.Transitions.CONSUME_BUFFER_1);
-				pene.fire(PN.Transitions.FINISHED_CONSUMING_BUFFER_1);
+		for (int i = 0; i<20; i++) {
+			if (pene.isTransitionEnabled(PN.Transitions.PRODUCE_BUFFER_1)) {
+				System.out.println("Puedo producir buffer 1 numero: " + i);
+				pene.fire(PN.Transitions.PRODUCE_BUFFER_1);
+				pene.fire(PN.Transitions.FINISHED_PRODUCING_BUFFER_1);
 			}
 			else {
-				System.out.println("No puedo producir en buffer 1");
+				System.out.println("No puedo producir en buffer 1 numero: " + i);
 			}
-			if (pene.isTransitionEnabled(PN.Transitions.CONSUME_BUFFER_2)) {
-				System.out.println("Puedo consumir buffer 2");
-				pene.fire(PN.Transitions.CONSUME_BUFFER_2);
-				pene.fire(PN.Transitions.FINISHED_CONSUMING_BUFFER_2);
+			if (pene.isTransitionEnabled(PN.Transitions.PRODUCE_BUFFER_2)) {
+				System.out.println("Puedo producir buffer 2 numero: " + i);
+				pene.fire(PN.Transitions.PRODUCE_BUFFER_2);
+				pene.fire(PN.Transitions.FINISHED_PRODUCING_BUFFER_2);
 			}
 			else {
-				System.out.println("No puedo producir en buffer 2");
+				System.out.println("No puedo producir en buffer 2 numero: " + i);
 			}
 		}
 	}
