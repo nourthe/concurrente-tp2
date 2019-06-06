2175de29ecfa6bdc86c7c08b80e5434d7512717d
jerebntz <jerebntz@gmail.com>
2019-06-06 14:46:50 -0300

    new class to avoid overuse of if statements (can be improved)

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Queue;
 
 public class Recipe {
+    private PN.Transitions[] transitions;
+    private Queue<String> buffer;
+
+    Recipe(Queue<String> buffer, PN.Transitions... transitions) {
+        this.transitions = transitions;
+        this.buffer = buffer;
+    }
+
+    public PN.Transitions[] getTransitions() {
+        return this.transitions;
+    }
+
+    public Queue<String> getBuffer() {
+        return this.buffer;
+    }
 }
