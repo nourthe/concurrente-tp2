cce376bdf3a68c05cf6a01a788574540e6e5fe98
jerebntz <jerebntz@gmail.com>
2019-06-06 14:42:57 -0300

    updated PNProducer and PNConsumer instantiation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.stream.IntStream;
 
 public class Main {
@@ -10,11 +12,15 @@ public class Main {
         // new PNtest();
         Monitor monitor = new Monitor();
 
+        // Buffers
+        Queue<String> buffer1 = new LinkedList<>();
+        Queue<String> buffer2 = new LinkedList<>();
+
         // Producers
         List<Thread> producerThreadList = new ArrayList<>();
 
         IntStream.range(0,NUMBER_OF_PRODUCERS).forEachOrdered(i -> {
-            producerThreadList.add(new PNProducer(monitor, "Producer-"+i));
+            producerThreadList.add(new PNProducer(monitor, "Producer-"+i, buffer1, buffer2,false));
             producerThreadList.get(i).start();
         });
 
@@ -22,13 +28,13 @@ public class Main {
         List<Thread> consumerThreadList = new ArrayList<>();
 
         IntStream.range(0,NUMBER_OF_CONSUMERS).forEachOrdered(i -> {
-            consumerThreadList.add(new PNConsumer(monitor, "Consumer-"+i));
+            consumerThreadList.add(new PNConsumer(monitor, "Consumer-"+i, buffer1, buffer2));
             consumerThreadList.get(i).start();
         });
 
         // Loger create and start.
-        Loger loger = new Loger(monitor, producerThreadList, consumerThreadList, "out/log.txt");
-        loger.start();
+        //Loger loger = new Loger(monitor, producerThreadList, consumerThreadList, "out/log.txt");
+        //loger.start();
 
         producerThreadList.forEach(thread -> {
             try {
@@ -39,9 +45,8 @@ public class Main {
         });
 
         System.out.println("Producidos todos los items\n");
-        System.out.println(monitor.getBuffersLoad());
 
-        while (monitor.getBuffersLoad() != 0){
+        while ((buffer1.size() & buffer2.size()) != 0){
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -51,6 +56,6 @@ public class Main {
 
         System.out.println("Consumidos todos los items\n");
         consumerThreadList.forEach(Thread::interrupt);
-        loger.interrupt();
+        //loger.interrupt();
     }
 }
